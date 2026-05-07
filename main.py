258cf810ffc1bf970bef90e55478e0c5511d440b
Lukas Kamen <kamen.lukas1990@gmail.com>
2026-05-07 20:32:21 +0200

    imports

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from PIL import Image
+from pytesseract import pytesseract
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import time
+import os
+
+findCardModel = YOLO("findCard.pt")
+
+
 def main():
     print("Hello from pokemon-card-merger!")
 
